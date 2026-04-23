61e50a0f57f1f549489aee41886640c721866b53
Gaetan  Cariou <gaetancariou@MacBook-Air-de-Gaetan.local>
2026-04-23 20:42:29 +0200

    Unoptimized images fix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
-    formats: ['image/avif', 'image/webp'],
   },
   transpilePackages: ['gsap'],
 };
